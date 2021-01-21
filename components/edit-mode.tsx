b5a61fc3e3df1748235e05c7083469b8ef8dc1ab
Michael Hopcroft <mhop@microsoft.com>
2021-01-20 23:27:44 -0800

    WIP: integrate @monaco-editor/react

--- components/edit-mode.tsx
+++ components/edit-mode.tsx
@@ -1,3 +1,22 @@
+import Editor from "@monaco-editor/react";
+
+// TODO
+//   Dark mode
+//   Wrap Editor inside of another component that does Redux dispatch on componentWillUnmount()
+//   Save/Update/Analyze button button
+//   Dirty buffer detection
+//   YAML syntax highlighting
+//   Instructions to user
+//   Help pane
 export default function EditMode() {
-  return <h2>Edit Mode</h2>;
+  return (
+    <div>
+      <h2>Edit Mode</h2>
+      <Editor
+        height="90vh"
+        defaultLanguage="yaml"
+        defaultValue="// some comment"
+      />
+    </div>
+  );
 }
